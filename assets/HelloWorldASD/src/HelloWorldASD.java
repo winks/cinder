dcbcaefbc0f6db5ebe50fc58b74685560b28584d
Florian Anderiasch <fa@art-core.org>
2009-05-10 11:17:01 +0200

    added HelloWorldASD and cinder.(properties|xml) to assets

---
+++ assets/HelloWorldASD/src/HelloWorldASD.java
@@ -0,0 +1,10 @@
+import java.io.File;
+import java.io.FileInputStream;
+
+public class HelloWorldASD {
+	//error();
+	public static void main(String[] args) {
+		HelloWorldOutput hwo = new HelloWorldOutput();
+		hwo.say();
+	}
+}
---
+++ assets/HelloWorldASD/src/HelloWorldOutput.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class HelloWorldOutput {
+	public void say() {
+		ArrayList<String> al = new ArrayList<String>();
+		al.add("Hello, ");
+		al.add("World!");
+		al.add("\n");
+		for (String s : al) {
+			System.out.print(s);
+		}
+	}
+}
